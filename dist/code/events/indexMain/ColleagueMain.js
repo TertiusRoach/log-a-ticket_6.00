867d23fa1fe7004c8308feb21249d21f10af1cff
Tertius Roach <tertius.roach@gmail.com>
2021-12-23 13:41:10 +0200

    Replaced main header text with active colleague

--- dist/code/events/indexMain/ColleagueMain.js
+++ dist/code/events/indexMain/ColleagueMain.js
@@ -9,7 +9,11 @@ define(["require", "exports"], function (require, exports) {
                 var indexBody = document.querySelector('#index-body');
                 var indexHeader = document.querySelector('#index-header');
                 var indexMain = document.querySelector('#index-main');
+                var mainHeader = indexMain.querySelector('#colleague-name');
                 var indexSidebar = document.querySelector('#index-sidebar');
+                var activeEmployee = document.querySelector('.active-colleague');
+                var activeColleague = activeEmployee.querySelector('.text').textContent;
+                mainHeader.textContent = "".concat(activeColleague);
                 var indexOverlay = document.querySelector('#index-overlay');
                 var indexData = document.querySelector('#index-data');
                 console.log('--ColleagueMain.js Loaded');
--- src/code/events/indexMain/ColleagueMain.ts
+++ src/code/events/indexMain/ColleagueMain.ts
@@ -5,10 +5,20 @@ export namespace ColleagueMain {
       // First ▼ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= ◄ */
       // Declarations ▼ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ◄ */
       const indexBody: HTMLBodyElement = document.querySelector('#index-body');
+
       const indexHeader: HTMLElement = document.querySelector('#index-header');
+
       const indexMain: HTMLElement = document.querySelector('#index-main');
+      let mainHeader: HTMLElement = indexMain.querySelector('#colleague-name');
+
       const indexSidebar: HTMLElement = document.querySelector('#index-sidebar');
+      let activeEmployee: HTMLElement = document.querySelector('.active-colleague');
+      let activeColleague: String = activeEmployee.querySelector('.text').textContent;
+      mainHeader.textContent = `${activeColleague}`;
+      console.log('Replaced main header with active colleague');
+
       const indexOverlay: HTMLElement = document.querySelector('#index-overlay');
+
       const indexData: HTMLElement = document.querySelector('#index-data');
 
       // Functions ▼ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ◄ */
