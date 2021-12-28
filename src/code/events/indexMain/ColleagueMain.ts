f063cd624ac16969585463a52d0debe79d3cb44c
Tertius Roach <tertius.roach@gmail.com>
2021-12-28 10:11:54 +0200

    Count tickets based on active tab: Check

--- src/code/events/indexMain/ColleagueMain.ts
+++ src/code/events/indexMain/ColleagueMain.ts
@@ -43,6 +43,59 @@ export namespace ColleagueMain {
       const indexData: HTMLElement = document.querySelector('#index-data');
 
       /* Functions ▼ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ◄ */
+      function countTickets(tab: 'pending' | 'assigned' | 'resolved' | 'deleted' | 'everything' | 'active') {
+        let ticketsNotification: HTMLElement = indexMain.querySelector('#colleague-notification h2');
+        let ticketsContainer: HTMLElement = indexMain.querySelector('#tickets-container');
+
+        switch (tab) {
+          case 'pending':
+            let pendingTotal: Number = ticketsContainer.getElementsByClassName('pending').length;
+            ticketsNotification.style.background = `${GetColor.pendingDefault()}`;
+            ticketsNotification.textContent = `${pendingTotal}`;
+            break;
+          case 'assigned':
+            let assignedTotal: Number = ticketsContainer.getElementsByClassName('assigned').length;
+            ticketsNotification.style.background = `${GetColor.assignedDefault()}`;
+            ticketsNotification.textContent = `${assignedTotal}`;
+            break;
+          case 'resolved':
+            let resolvedTotal: Number = ticketsContainer.getElementsByClassName('resolved').length;
+            ticketsNotification.style.background = `${GetColor.resolvedDefault()}`;
+            ticketsNotification.textContent = `${resolvedTotal}`;
+            break;
+          case 'deleted':
+            let deletedTotal: Number = ticketsContainer.getElementsByClassName('deleted').length;
+            ticketsNotification.style.background = `${GetColor.deletedDefault()}`;
+            ticketsNotification.textContent = `${deletedTotal}`;
+            break;
+          case 'everything':
+            let everythingTotal: Number = ticketsContainer.getElementsByTagName('article').length;
+            ticketsNotification.style.background = `${GetColor.primaryDark()}`;
+            ticketsNotification.textContent = `${everythingTotal}`;
+            break;
+          case 'active':
+            let activeTab: String = ticketsContainer.classList[0];
+            let activeStatus: String = activeTab.split('-')[0];
+            let activeTotal: Number = ticketsContainer.getElementsByClassName(`${activeStatus}`).length;
+            let backgroundColor: String;
+
+            if (activeStatus === 'pending') {
+              backgroundColor = GetColor.pendingDefault();
+            } else if (activeStatus === 'assigned') {
+              backgroundColor = GetColor.assignedDefault();
+            } else if (activeStatus === 'resolved') {
+              backgroundColor = GetColor.resolvedDefault();
+            } else if (activeStatus === 'deleted') {
+              backgroundColor = GetColor.deletedDefault();
+            } else if (activeStatus === 'everything') {
+              backgroundColor = GetColor.primaryDark();
+            }
+
+            ticketsNotification.style.background = `${backgroundColor}`;
+            ticketsNotification.textContent = `${activeTotal}`;
+            break;
+        }
+      }
 
       /* Classes ▼ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ◄ */
 
@@ -54,35 +107,46 @@ export namespace ColleagueMain {
         new GetEvent.forPage(`user-${activeStatus}`, GetPath.forHTML('overlay'));
         indexOverlay.style.display = 'grid';
       });
+      $(openEmployees).on('click', () => {
+        indexSidebar.style.display = 'grid';
+      });
 
       $(assignedTab)
         .on('click', () => {
           ticketsMain.className = 'assigned-tickets';
         })
-        .on('mouseenter', () => {})
-        .on('mouseleave', () => {});
+        .on('mouseenter', () => {
+          countTickets('assigned');
+        })
+        .on('mouseleave', () => {
+          countTickets('active');
+        });
 
       $(resolvedTab)
         .on('click', () => {
           ticketsMain.className = 'resolved-tickets';
         })
-        .on('mouseenter', () => {})
-        .on('mouseleave', () => {});
+        .on('mouseenter', () => {
+          countTickets('resolved');
+        })
+        .on('mouseleave', () => {
+          countTickets('active');
+        });
 
       $(deletedTab)
         .on('click', () => {
           ticketsMain.className = 'deleted-tickets';
         })
-        .on('mouseenter', () => {})
-        .on('mouseleave', () => {});
-      $(openEmployees).on('click', () => {
-        indexSidebar.style.display = 'grid';
-      });
+        .on('mouseenter', () => {
+          countTickets('deleted');
+        })
+        .on('mouseleave', () => {
+          countTickets('active');
+        });
 
       /* Last ▼ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ◄ */
+      countTickets('active');
       /*--► console.log('--ColleagueMain.js Loaded'); ◄--*/
-
-      console.log('--ColleagueMain.js Loaded');
     }
   }
 }
--- src/code/events/indexMain/UserMain.ts
+++ src/code/events/indexMain/UserMain.ts
@@ -40,6 +40,59 @@ export namespace UserMain {
       const indexData: HTMLElement = document.querySelector('#index-data');
 
       /* Functions ▼ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= ◄ */
+      function countTickets(tab: 'pending' | 'assigned' | 'resolved' | 'deleted' | 'everything' | 'active') {
+        let ticketsNotification: HTMLElement = indexMain.querySelector('#user-notification h2');
+        let ticketsContainer: HTMLElement = indexMain.querySelector('#tickets-container');
+
+        switch (tab) {
+          case 'pending':
+            let pendingTotal: Number = ticketsContainer.getElementsByClassName('pending').length;
+            ticketsNotification.style.background = `${GetColor.pendingDefault()}`;
+            ticketsNotification.textContent = `${pendingTotal}`;
+            break;
+          case 'assigned':
+            let assignedTotal: Number = ticketsContainer.getElementsByClassName('assigned').length;
+            ticketsNotification.style.background = `${GetColor.assignedDefault()}`;
+            ticketsNotification.textContent = `${assignedTotal}`;
+            break;
+          case 'resolved':
+            let resolvedTotal: Number = ticketsContainer.getElementsByClassName('resolved').length;
+            ticketsNotification.style.background = `${GetColor.resolvedDefault()}`;
+            ticketsNotification.textContent = `${resolvedTotal}`;
+            break;
+          case 'deleted':
+            let deletedTotal: Number = ticketsContainer.getElementsByClassName('deleted').length;
+            ticketsNotification.style.background = `${GetColor.deletedDefault()}`;
+            ticketsNotification.textContent = `${deletedTotal}`;
+            break;
+          case 'everything':
+            let everythingTotal: Number = ticketsContainer.getElementsByTagName('article').length;
+            ticketsNotification.style.background = `${GetColor.primaryDark()}`;
+            ticketsNotification.textContent = `${everythingTotal}`;
+            break;
+          case 'active':
+            let activeTab: String = ticketsContainer.classList[0];
+            let activeStatus: String = activeTab.split('-')[0];
+            let activeTotal: Number = ticketsContainer.getElementsByClassName(`${activeStatus}`).length;
+            let backgroundColor: String;
+
+            if (activeStatus === 'pending') {
+              backgroundColor = GetColor.pendingDefault();
+            } else if (activeStatus === 'assigned') {
+              backgroundColor = GetColor.assignedDefault();
+            } else if (activeStatus === 'resolved') {
+              backgroundColor = GetColor.resolvedDefault();
+            } else if (activeStatus === 'deleted') {
+              backgroundColor = GetColor.deletedDefault();
+            } else if (activeStatus === 'everything') {
+              backgroundColor = GetColor.primaryDark();
+            }
+
+            ticketsNotification.style.background = `${backgroundColor}`;
+            ticketsNotification.textContent = `${activeTotal}`;
+            break;
+        }
+      }
 
       /* Events ▼ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= ◄ */
       $(ticketsMain).on('click', () => {
@@ -57,24 +110,37 @@ export namespace UserMain {
         .on('click', () => {
           ticketsMain.className = 'assigned-tickets';
         })
-        .on('mouseenter', () => {})
-        .on('mouseleave', () => {});
+        .on('mouseenter', () => {
+          countTickets('assigned');
+        })
+        .on('mouseleave', () => {
+          countTickets('active');
+        });
 
       $(resolvedTab)
         .on('click', () => {
           ticketsMain.className = 'resolved-tickets';
         })
-        .on('mouseenter', () => {})
-        .on('mouseleave', () => {});
+        .on('mouseenter', () => {
+          countTickets('resolved');
+        })
+        .on('mouseleave', () => {
+          countTickets('active');
+        });
 
       $(deletedTab)
         .on('click', () => {
           ticketsMain.className = 'deleted-tickets';
         })
-        .on('mouseenter', () => {})
-        .on('mouseleave', () => {});
+        .on('mouseenter', () => {
+          countTickets('deleted');
+        })
+        .on('mouseleave', () => {
+          countTickets('active');
+        });
 
       /* Last ▼ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= ◄ */
+      countTickets('active');
       /*--► console.log('--UserMain.js Loaded'); ◄--*/
     }
   }
